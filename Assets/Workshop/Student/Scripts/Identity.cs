fd49b8d8cc8f44d03fb17110faf231873368becc
Gittanon <gittanon.watt@bumail.net>
2026-09-01 17:34:15 +0700

    workshop

--- Assets/Workshop/Student/Scripts/Identity.cs
+++ Assets/Workshop/Student/Scripts/Identity.cs
@@ -9,4 +9,15 @@ public class Identity : MonoBehaviour
     public int positionX;
     public int positionY;
     public OOPMapGenerator mapGenerator;
+
+    public void PrintInfo()
+    {
+        Debug.Log("Tell me Your"+Name);
+    }
+
+    public virtual void Hit()
+    {
+    
+    }
 }
+
